99d13cf7ace80abffb7717681cbfb158692631f3
Alby Ajin <180402021+AABatteries6@users.noreply.github.com>
2026-08-01 20:24:50 +0100

    Get and validate
    Tidied and tested get_folder(), completed validate_folder(), created main() which is currently used to test helper functions made.

--- main.py
+++ main.py
@@ -2,5 +2,27 @@ from pathlib import Path
 import shutil
 
 def get_folder():
+    # This method takes a filepath input and converts to Path object
     folder = Path(input("Enter folder path:  "))
     return folder
+
+def validate_folder(folder: Path):
+    # Validates inputted filepath
+    if (folder.exists() and folder.is_dir()):
+        return True
+    elif (not folder.exists()):
+        print("Folder cannot be found.")
+        return False
+    else:
+        print("Path points to file, not folder.")
+        return False
+
+def main():
+    folder = get_folder()
+    print(folder)
+    print(type(folder))
+    validate_folder(folder)
+
+
+if __name__ == "__main__":
+    main()
